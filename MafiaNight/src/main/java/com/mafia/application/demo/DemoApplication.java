a16e98832f3447d676b42ac213faf1c18ba07e81
Nikhil Kaushik <nk2214@rit.edu>
2020-11-16 22:25:15 -0500

    New UI + CORS update to Spring Boot

--- MafiaNight/src/main/java/com/mafia/application/demo/DemoApplication.java
+++ MafiaNight/src/main/java/com/mafia/application/demo/DemoApplication.java
@@ -2,13 +2,20 @@ package com.mafia.application.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 
 //@ComponentScan({"com.mafia.application.controller", "com.mafia.application.service.PlayerService"})
 @ComponentScan(basePackages = {"com.mafia.application"})
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+	
+	@Override
+	   protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+	       return builder.sources(DemoApplication.class);
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- demo/src/main/java/com/mafia/application/controller/TestController.java
+++ demo/src/main/java/com/mafia/application/controller/TestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,18 +32,21 @@ import com.mafia.application.service.PlayerService;
 public class TestController {
 	@Autowired
 	private PlayerService playerService;
-
+	
+	@CrossOrigin
 	@GetMapping(value = "/v1/signup")
 	public boolean playerSignup(@RequestBody Player p) {
 		return playerService.createPlayer(p);
 	}
 	
+	@CrossOrigin
 	@PostMapping(value = "/v1/host")
 	public GameRoom hostGame(@RequestBody PlayerProfile pf) {
 		GameRoom obj = playerService.hostGame(pf); 
 		return obj;
 	}
 	
+	@CrossOrigin
 	@PostMapping(value = "/v1/test")
 	public List<Player> testGame(@RequestBody PlayerProfile pf) {
 		List<Player> l1 = new ArrayList<>();
@@ -51,11 +55,13 @@ public class TestController {
 		return l1;
 	}
 	
+	@CrossOrigin
 	@PostMapping(value = "/v1/join/{gameId}")
 	public GameRoom joinGame(@PathVariable String gameId, @RequestBody PlayerProfile pf) {
 		return playerService.joinGame(gameId, pf);
 	}
 	
+	@CrossOrigin
 	@GetMapping(value = "/v1/updateGame/{gameId}")
 	public GameRoom getGame(@PathVariable String gameId) {
 		return playerService.getGame(gameId);
@@ -63,23 +69,26 @@ public class TestController {
 	
 	
 	
-	
+	@CrossOrigin
 	@PostMapping(value = "/gamePlay/startGame/{gameId}")
 	public GamePlay startGame(@PathVariable String gameId) {
 		GamePlay ret = playerService.startGame(gameId); 
 		return null;
 	}
 	
+	@CrossOrigin
 	@GetMapping(value = "/gamePlay/refreshSelection/{gameId}")
 	public Map<String, List<String>> refreshGame(@PathVariable String gameId) {
 		return playerService.currentGame(gameId);
 	}
 	
+	@CrossOrigin
 	@GetMapping(value = "/gamePlay/refreshSelection1/{gameId}")
 	public GamePlay refreshGame1(@PathVariable String gameId) {
 		return playerService.currentGameObj(gameId);
 	}
 	
+	@CrossOrigin
 	@PutMapping(value = "/gamePlay/updateSelection/{gameId}/{pid}/{sid}")
 	public void gameSelection(@PathVariable String gameId, @PathVariable String pid, @PathVariable String sid) {
 		playerService.updateSelection(gameId, pid, sid);
